268e8849dcc4a61c6bd270902c670414ab9037b1
baizemin <10023546cgk@gmail.com>
2025-09-13 20:26:07 +0530

    search algo's

---
+++ remove_duplicates_sorted.py
@@ -0,0 +1,9 @@
+def remove_duplicates(arr):
+    j = 0
+    for i in range(1,len(arr)):
+        if arr[i] != arr[j]:
+            j+=1
+            arr[j] = arr[i]
+arr = [1,2,2,2,3,3,4,5]
+remove_duplicates(arr)
+print(arr)
